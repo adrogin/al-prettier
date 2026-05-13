e00bd34745b80297adfd4002f0d8b06d0887b6c6
Alexander Drogin <adrogin@outlook.com>
2026-05-13 21:55:40 +0200

    SourceTableView property, EnumExtension object, ErrorListener in parser

---
+++ tests/al-printer.enumextension.test.js
@@ -0,0 +1,46 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import antlr4 from 'antlr4';
+import ALParser from '../../algrammar/JS/ALParser.js';
+import printer from '../plugin/printer.js';
+import * as prettier from "prettier";
+import { alFormat } from './testUtils.js';
+
+describe('Printing EnumExtension objects', () => {
+  it('Empty EnumExtension object', () => {
+    const code = `
+enumextension 50000 MyExtension extends "Customer Blocked"
+{}`;
+
+    const expected = `enumextension 50000 MyExtension extends "Customer Blocked"
+{
+}
+`;
+
+    return alFormat(code).then(formattedCode =>
+      expect(formattedCode).to.equal(expected))
+  });
+
+  it('EnumExtension and extended object with namespaces', () => {
+    const code = `
+namespace MyNamespace;
+enumextension 50000 MyExtension extends Microsoft.Sales.Customer."Customer Blocked"
+{
+  value(60000; Assembly) { Caption='Assembly'; }
+}`;
+
+    const expected = `namespace MyNamespace;
+
+enumextension 50000 MyExtension extends Microsoft.Sales.Customer."Customer Blocked"
+{
+  value(60000; Assembly)
+  {
+    Caption = 'Assembly';
+  }
+}
+`;
+
+    return alFormat(code).then(formattedCode =>
+      expect(formattedCode).to.equal(expected))
+  });
+});
