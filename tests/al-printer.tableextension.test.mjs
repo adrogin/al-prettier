1010e7f37883a18043bc55c749a4005eb42d6c6d
Alexander Drogin <adrogin@outlook.com>
2026-05-31 21:28:04 +0200

    Changed tests to .mjs

--- tests/al-printer.tableextension.test.mjs
+++ tests/al-printer.tableextension.test.mjs
@@ -1,6 +1,6 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
-import { alFormat } from './testUtils.js';
+import { alFormat } from './testUtils.mjs';
 
 describe('Table extension object', () => {
     it('Table extension adding one field and modifying another', () => {
